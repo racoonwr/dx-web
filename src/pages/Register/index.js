ea338ff28f0ffc61d56c96f66cc9203b258d17f1
Your Name <you@example.com>
2021-04-07 23:20:02 +0800

    register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { Button, InputItem, List } from "antd-mobile";
 
+import "./index.less";
+
+const FIELDITEMS = [{
+  name: "name",
+  label: "用户名",
+  placeholder: "请输入用户名"
+}, {
+  name: "pwd",
+  label: "密码",
+  placeholder: "请输入密码",
+  type: "password"
+}, {
+  name: "name2",
+  label: "经办人姓名",
+  placeholder: "请输入经办人姓名"
+}, {
+  name: "phone",
+  label: "经办人电话",
+  placeholder: "请输入经办人电话",
+  type: "phone"
+}, {
+  name: "captcha",
+  label: "验证码",
+  placeholder: "请输入验证码",
+  type: "number"
+}];
+
 export default () => {
   return <div className={"page"}>
     <div className={"page-register"}>
-      <div>
-        1111111
+      <div className={"register-logo"}>
+        注册
       </div>
+      <List>
+        {
+          FIELDITEMS.map(e => {
+            return <InputItem>
+              {e.label}
+            </InputItem>;
+          })
+        }
+      </List>
     </div>
   </div>;
 }
