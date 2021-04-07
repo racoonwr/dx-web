e267fe3f5fd495b01d7550b81157b16265a564ee
ShiEnpei <qOC8d0sKyPEZpcSI_nzi>
2021-04-07 18:32:30 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import "lib-flexible";
+import { Toast } from "antd-mobile";
+
+console.log("_____>", process.env.apiUrl);
+
+Toast.config({
+  mask: false
+});
---
+++ src/models/common.js
@@ -0,0 +1,16 @@
+import { Effect } from "umi";
+// import { getUrlMap } from "../services/tool";
+
+const Model = {
+  namespace: "common",
+  state: {},
+  effects: {
+    /* 分享链接 */
+    // * getUrlMap({ payload }, { call }) {
+    //   return yield call(getUrlMap, payload);
+    // }
+  },
+  reducers: {}
+};
+
+export default Model;
